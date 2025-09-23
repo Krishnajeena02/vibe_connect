83a462a474af7b35eb8df8f7f393c9d96a5ee583
krishna jeena02 <krishjeena8700745775@gmail.com>
2025-09-23 22:39:30 +0530

    ready to deployment

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,3 +1,5 @@
+
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -5,10 +7,20 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
+module.exports = {
+  extends: "next/core-web-vitals",
+  rules: {
+    "react/no-unescaped-entities": "off"
+  }
+}
 
 const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
 export default eslintConfig;
+
+
+ 
