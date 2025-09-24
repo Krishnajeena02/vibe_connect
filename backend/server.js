64a1d52b1af778d07af412d97f027c6590110d05
krishna jeena02 <krishjeena8700745775@gmail.com>
2025-09-24 10:52:34 +0530

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,19 @@ import authRoutes from'./routes/auth.routes.js';
 const app = express();
 const server = http.createServer(app);
 app.use(express.urlencoded({ extended: true }));
+const allowedOrigins = [
+  "http://localhost:3000", // local
+  process.env.FRONTEND_URL,
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 }));
 app.use(express.json());
