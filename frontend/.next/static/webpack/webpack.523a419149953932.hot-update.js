6bde7e021f02ba28ef510648b309909041571d64
krishna jeena02 <krishjeena8700745775@gmail.com>
2025-09-24 10:18:43 +0530

    ready to deploy

--- frontend/.next/static/webpack/webpack.523a419149953932.hot-update.js
+++ frontend/.next/static/webpack/webpack.523a419149953932.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("f7e23ed4a7e60ca9")
+/******/ 	__webpack_require__.h = () => ("1514ef7134d0710f")
 /******/ })();
 /******/ 
 /******/ }
