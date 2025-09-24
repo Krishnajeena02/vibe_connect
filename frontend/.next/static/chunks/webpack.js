b7268b13fa5c65c5306723b030fa0ca924e54879
krishna jeena02 <krishjeena8700745775@gmail.com>
2025-09-24 11:50:35 +0530

    ready to deploy

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -181,7 +181,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("a6f84b3b200e847a")
+/******/ 		__webpack_require__.h = () => ("91a887274af3f038")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
